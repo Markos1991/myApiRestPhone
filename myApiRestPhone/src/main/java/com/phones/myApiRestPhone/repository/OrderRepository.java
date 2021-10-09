43c6c388606e9f3c5ac0673fff2ef89bc8192739
Markos1991 <marcos.rafael@ucm.es>
2021-10-09 13:53:18 +0200

    Delete myApiRestPhone directory

--- myApiRestPhone/src/main/java/com/phones/myApiRestPhone/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.phones.myApiRestPhone.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.phones.myApiRestPhone.model.Order;
-
-public interface OrderRepository extends MongoRepository<Order, String>{
-
-}
